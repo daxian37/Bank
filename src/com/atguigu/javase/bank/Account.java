8b969f622d9127e5f40282d5c789a8cee52f229b
daxian37 <daxian37@126.com>
2018-03-18 22:22:07 +0800

    creating TestBanking file

--- src/com/atguigu/javase/bank/Account.java
+++ src/com/atguigu/javase/bank/Account.java
@@ -1,4 +1,27 @@
 package com.atguigu.javase.bank;
 
 public class Account {
+    private double balance;
+
+
+    public Account(double init_balance) {
+        balance = init_balance;
+    }
+
+    public double getBalance(){
+        return balance;
+
+    }
+
+    public void deposit(double amt){
+        balance += amt;
+    }
+
+    public void withdrow(double amt){
+
+        balance -= amt;
+    }
+
 }
+
+

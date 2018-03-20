803735ae94c62f1f759585a4e6ae600114036eee
daxian37 <daxian37@126.com>
2018-03-20 23:26:50 +0800

    build lesson5-1code

---
+++ src/com/atguigu/javase/bank51/Bank.java
@@ -0,0 +1,53 @@
+package com.atguigu.javase.bank51;
+
+/**
+ *
+ * 提示:
+ 对银行来说，可添加 Bank 类。 Bank 对象跟踪自身与其客户间的关系。用 Customer 对象的数组实现这个集合化的关系。还要保持一个整数属性来跟踪 银 行当前有多少客户。
+ a.
+ c.
+ g. 编译并运行 TestBanking 程序。可以看到下列输出结果: Customer [1] is Simms,Jane
+ Customer [2] is Bryant,Owen
+ Customer [3] is Soley,Tim
+ Customer [4] is Soley,Maria**/
+
+//创建 Bank 类
+public class Bank {
+
+    // b. 为 Bank 类 增 加 两 个 属 性 : customers(Customer对象的数组 )
+    // 和numberOfCustomers(整数，跟踪下一个 customers 数组索引)
+
+    private Customer[] customers;
+    private int numberOfCustomers;
+
+    //添加公有构造器，以合适的最大尺寸(至少大于 5)初始化 customers 数组。
+    public Bank() {
+        customers = new Customer[10];
+
+    }
+
+    // d. 添加 addCustomer 方法。该方法必须依照参数(姓，名)构造一个新的 Customer
+    // 对象然后把它放到 customer 数组中。
+    // 还必须把 numberofCustomers 属性的值加 1。
+
+    public void addCustomer(String f,String l){
+
+        Customer customer = new Customer(f, l);
+        customers[numberOfCustomers] =customer;
+        numberOfCustomers++;
+    }
+
+
+    // e. 添加 getNumOfCustomers 访问方法，它返回 numberofCustomers 属 性值。
+
+
+    public int getNumOfCustomers() {
+        return numberOfCustomers;
+    }
+
+    // f. 添加 getCustomer方法。它返回与给出的index参数相关的客户。
+
+    public Customer getCustomer(int index) {
+        return customers[index];
+    }
+}
---
+++ src/com/atguigu/javase/bank51/SavingAccount.java
@@ -0,0 +1,19 @@
+package com.atguigu.javase.bank51;
+
+
+/**
+ * 创建 Account 类的两个子类:SavingAccount 和 CheckingAccount 子类
+ a. 修改 Account 类;将 balance 属性的访问方式改为 protected
+ b. 创建 SavingAccount 类，该类继承 Account 类
+ c. 该类必须包含一个类型为 double 的 interestRate 属性
+ d. 该类必须包括带有两个参数(balance 和 interest_rate)的公有构造器。该 构 造器必须通过调用
+ *
+ *
+ * **/
+public class SavingAccount extends Account {
+    public double interestRate;
+    public SavingAccount(double init_balance,double interestRate) {
+        super(init_balance);
+        this.interestRate = interestRate;
+    }
+}
